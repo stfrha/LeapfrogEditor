ccf4d7d3472a0d869f2dd69cb9007300a376c932
stfrha <mail@fredrikhoffman.se>
2018-06-25 20:52:01 +0200

    Added some files

---
+++ ValueConverters/EllipseCenterValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LeapfrogEditor
+{
+   class EllipseCenterValueConverter : IMultiValueConverter
+   {
+      public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+      {
+         if (values.Count() == 2)
+         {
+            if ((values[0] is double) && (values[1] is double))
+            {
+               double pos = (double)values[0];
+               double radius = (double)values[1];
+
+               return pos - radius;
+            }
+         }
+
+         return null;
+      }
+
+      public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
---
+++ ViewModels/LfPointViewModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LeapfrogEditor
+{
+    class LfPointViewModel : MicroMvvm.ViewModelBase, IPositionInterface
+   {
+      #region Declarations
+
+      private double _posX;
+      private double _posY;
+
+      private MainViewModel _mainVm;
+      private LfStaticBoxViewModel _parent;
+
+      private bool _isSelected;
+
+
+      #endregion
+
+      #region Constructors
+
+      public LfPointViewModel(MainViewModel mainVm, LfStaticBoxViewModel parent, Point p)
+      {
+         PosX = p.X;
+         PosY = p.Y;
+         MainVm = mainVm;
+         Parent = parent;
+         IsSelected = false;
+      }
+
+      #endregion
+
+      #region Properties
+
+      public MainViewModel MainVm
+      {
+         get { return _mainVm; }
+         set { _mainVm = value; }
+      }
+
+      public LfStaticBoxViewModel Parent
+      {
+         get { return _parent; }
+         set
+         {
+            _parent = value;
+            OnPropertyChanged("");
+         }
+      }
+
+      public double PosX
+      {
+         get
+         {
+            return _posX;
+         }
+         set
+         {
+            _posX = value;
+            OnPropertyChanged("PosX");
+         }
+      }
+
+
+      public double PosY
+      {
+         get
+         {
+            return _posY;
+         }
+         set
+         {
+            _posY = value;
+            OnPropertyChanged("PosY");
+         }
+      }
+
+      public bool IsSelected
+      {
+         get { return _isSelected; }
+         set
+         {
+            _isSelected = value;
+            OnPropertyChanged("IsSelected");
+         }
+      }
+
+      #endregion
+   }
+}
