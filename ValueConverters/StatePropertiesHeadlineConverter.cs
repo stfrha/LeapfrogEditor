46fbebd25ea6b233f673e5da46ae82589a33b02d
stfrha <mail@fredrikhoffman.se>
2019-01-13 11:20:45 +0100

    added files

---
+++ ValueConverters/StatePropertiesHeadlineConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LeapfrogEditor
+{
+   class StatePropertiesHeadlineConverter : IValueConverter
+   {
+      public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+      {
+         if (value is ChildObjectStatePropertiesViewModel) 
+         {
+            ChildObjectStatePropertiesViewModel vm = (ChildObjectStatePropertiesViewModel)value;
+            ChildObjectViewModel chvm = vm.TreeParent as ChildObjectViewModel;
+
+            string s = chvm.Name + " State Properties";
+
+            return s;
+         }
+
+         return null;
+
+      }
+
+      public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+      {
+         return DependencyProperty.UnsetValue;
+      }
+   }
+
+}
